a24d05e1373b6fbfd38df04f2df1e99d82f42b55
Chelsea Spangler <chelseanspangler@gmail.com>
2020-06-15 14:25:25 -0700

    initial

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Chelsea Spangler is making a React Native app!</Text>
     </View>
   );
 }
