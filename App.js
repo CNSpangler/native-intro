063183b34d1242b65b1568a59939983e132d79d7
Chelsea Spangler <chelseanspangler@gmail.com>
2020-06-16 17:11:34 -0700

    homepage button console logs goal

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const handleInputChange = (enteredText) => setInput(enteredText);
+  const handlePress = () => console.log(input);
+  const handleAddGoal = () => setGoals([...goals]);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.screen}>
+    <View style={styles.screen}>
+      <View style={styles.container}>
         <TextInput 
           placeholder="Goals"
-          style={styles.input} />
-        <Button title="ADD" styles={styles.button} />
+          style={styles.input}
+          onChangeText={handleInputChange}
+          value={input}
+        />
+        <Button 
+          title="ADD" 
+          styles={styles.button}
+          onPress={handlePress}
+        />
       </View>
       <View>
 
       </View>
-      <Text>Testing</Text>
     </View>
   );
 }
@@ -31,13 +44,13 @@ const styles = StyleSheet.create({
   },
   
   button: {
-    backgroundColor: 'red'
+    color: 'red'
   },
 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
